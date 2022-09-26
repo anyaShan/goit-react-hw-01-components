8598de1c1d2ce3742bb99f2485ed7a9f2819b16a
anya.shan <anya.shanaida@yahoo.com>
2022-09-26 19:59:05 +0300

    css. Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,12 @@
 import { Description } from 'components/Profile/Description/Description';
 import { StatsList } from 'components/Profile/Stats/StatsList';
+import { ProfileCard } from 'components/Profile/Profile.styled';
 
 export const Profile = ({ user }) => {
   return (
-    <div class="profile">
+    <ProfileCard class="profile">
       <Description user={user} />
       <StatsList user={user} />
-    </div>
+    </ProfileCard>
   );
 };
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ProfileCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+
+  background-color: cadetblue;
+  border-radius: 5px;
+`;
